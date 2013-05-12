240e33ccd55daf6ab066d053379e2d5a97210393
Nicole Lewis <lewis.nicole@gmail.com>
2013-05-12 00:48:28 -0700

    finished mostly static sample app.  Updated rspec to :content

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -4,31 +4,31 @@ describe "Static pages::" do
   describe "Home page" do
     it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Sample App')
+      page.should have_selector('h1', :content => 'Sample App')
     end
 	it "should have the title 'Home'" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => 'Ruby on Rails Tutorial Sample App | Home')
+      page.should have_selector('title', :content => 'Ruby on Rails Tutorial Sample App | Home')
     end
   end
   describe "Help page" do
       it "should have the h1 'Help'" do
       visit '/static_pages/help'
-      page.should have_selector('h1', :text => 'Help')
+      page.should have_selector('h1', :content => 'Help')
     end
 	it "should have the title 'Help'" do
       visit '/static_pages/help'
-      page.should have_selector('title', :text => 'Ruby on Rails Tutorial Sample App | Help')
+      page.should have_selector('title', :content => 'Ruby on Rails Tutorial Sample App | Help')
     end
   end
   describe "About page" do
     it "should have the h1 'About Us'" do
       visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About Us')
+      page.should have_selector('h1', :content => 'About Us')
     end
 	it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      page.should have_selector('title', :text => 'Ruby on Rails Tutorial Sample App | About Us')
+      page.should have_selector('title', :content => 'Ruby on Rails Tutorial Sample App | About Us')
     end
   end
 end
